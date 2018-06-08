94719fed9b9c171d45d5655de87772c69c487d15
Dennis Yang <yzn311976@gmail.com>
2018-06-08 19:33:12 +0800

    101 submit

--- SymmetricTree101.java
+++ SymmetricTree101.java
@@ -30,6 +30,14 @@ Bonus points if you could solve it both recursively and iteratively.
     用两个队列，然后去广度优先 一层层遍历这个 树 
     第一个队列 每层从做往右 第二个队列 每层从右往左，最后两个队列如果相等 那么就说明 对称了
     问题来了，广度遍历怎么做来着。
+
+    看了一下 Solution 写一下吧，主要是当时考虑问题的时候，没搞清楚什么是 两个树对称，什么是自己对称
+    两棵树镜面对称
+    1.两个均为null true
+    2.一个为空，一个非空，false
+    3.两个节点值相等&&left的左子树，和right的右子树对称；&& left的右子树，和right的左子树对称
+    
+收获：想的太少了，思路不清晰，把两个树对称和一棵树自己镜面对称这两个概念区分开来，就已经解题了。
 */
 
 
@@ -44,6 +52,15 @@ Bonus points if you could solve it both recursively and iteratively.
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        
+        if (root == null)
+            return true;
+        return isMirror(root , root);
+    }
+    public boolean isMirror(TreeNode left , TreeNode right){
+        if(left == null && right == null)
+            return true;
+        if(left == null || right == null)
+            return false;
+        return (left.val==right.val)&&isMirror(left.right, right.left)&&isMirror(left.left, right.right);
     }
 }
